15508a70fca9a2c68fbbb1913b8792a7531934cb
Rozpi <joserozpide@yahoo.es>
2022-06-16 16:44:05 +0000

    cambio2160622

--- src/js/component/Todos/Components/ItemCounter.jsx
+++ src/js/component/Todos/Components/ItemCounter.jsx
@@ -4,7 +4,8 @@ const ItemCounter = ({ counter }) => {
 	return (
 		<div className="items-counter">
 			<small>
-				{counter} {`${counter === 1 ? "item" : "items"}`} left
+				{counter}{" "}
+				{`${counter === 1 ? "tarea pendiente" : "tareas pendientes "}`}
 			</small>
 		</div>
 	);
